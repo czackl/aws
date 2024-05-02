92139eb7778e44843bbc58565f799e294869b7a4
Constantin Zackl <constantin.zackl@gmail.com>
2024-05-02 15:57:50 +0200

    add date formatting

--- main.js
+++ main.js
@@ -53,6 +53,8 @@ async function showStations(url) {
         },
 
         onEachFeature: function (feature, layer) {
+            let pointInTime = new Date(feature.properties.date);
+            console.log(pointInTime);
             console.log(feature.properties);
             layer.bindPopup(`
                 <h4>${feature.properties.name} (${feature.geometry.coordinates[2]} m)</h4>
@@ -62,7 +64,7 @@ async function showStations(url) {
                     <li>Windgeschwindigkeit (km/h): ${feature.properties.WG || "-"} </li>
                     <li>Schneehöhe (cm): ${feature.properties.HS || "-"}</li>
                 </ul>
-            ${feature.properties.date}
+                <span>${pointInTime.toLocaleString()}</span>
             `);
 
         }
